75e4e95b5e3394fa428d172d246cf16ba6b8e585
ghalih <enghalih685@gmail.com>
2026-02-10 15:19:13 +0700

    final

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   base: ["/bookmark-landing-page/"],
+  build: {
+    outDir: "dist", // Ini memastikan hasil build masuk ke folder 'dist'
+  },
 });
